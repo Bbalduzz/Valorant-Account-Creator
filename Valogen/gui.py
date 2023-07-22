cd71d7ebb1eca3b4df6703f769e025b7cb633638
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2023-07-22 11:01:30 +0200

    Update gui.py
    support dark theme on windows

--- Valogen/gui.py
+++ Valogen/gui.py
@@ -1,13 +1,11 @@
 import sys
-from PyQt6.QtCore import QPoint, Qt, QEventLoop, QTimer, QSize, QRect, QThread, pyqtSignal
-from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtCore import Qt, QEventLoop, QTimer, QSize, QRect, QThread, pyqtSignal
+from PyQt6.QtGui import QIcon, QPalette, QColor
 from PyQt6.QtWidgets import  QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QFrame, QSpacerItem, QSizePolicy
-from qfluentwidgets import (InfoBarIcon, InfoBar, setTheme, Theme, InfoBarPosition, SplashScreen, CardWidget, setTheme, Theme, IconWidget, BodyLabel, CaptionLabel, PushButton,
+from qfluentwidgets import (InfoBarIcon, InfoBar, setTheme, Theme, InfoBarPosition, SplashScreen, CardWidget, Theme, IconWidget, BodyLabel, CaptionLabel, PushButton,
                             TransparentToolButton, FluentIcon, RoundMenu, Action, PushButton, StateToolTip, ComboBox)
-from qframelesswindow import FramelessWindow, StandardTitleBar
+from qframelesswindow import FramelessWindow
 
-# from src.chrome import RiotGen
-# from src.firefox import RiotGen
 from importlib import import_module
 
 class BotThread(QThread):
@@ -55,16 +53,6 @@ class CredentialCard(CardWidget):
     def copy_to_clipboard(self):
         clipboard = QApplication.clipboard()
         clipboard.setText(self.titleLabel.text())
-
-    def onMoreButtonClicked(self):
-        menu = RoundMenu(parent=self)
-        menu.addAction(Action(FluentIcon.SHARE, 'Share', self))
-        menu.addAction(Action(FluentIcon.CHAT, 'Write comment', self))
-        menu.addAction(Action(FluentIcon.PIN, 'Pin to taskbar', self))
-
-        x = (self.moreButton.width() - menu.sizeHint().width()) // 2 + 10
-        pos = self.moreButton.mapToGlobal(QPoint(x, self.moreButton.height()))
-        menu.exec(pos)
 
 class Credits(QWidget):
     def __init__(
@@ -239,7 +227,24 @@ class App(FramelessWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # Dark Theme for the framelesswindow
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
+    palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+    app.setPalette(palette)
+
     w = App()
     w.show()
     app.exec()
-
